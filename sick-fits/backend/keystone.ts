7a0d3a1bcd876d9f9375dc439887b40f8c05fcf3
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-06-29 13:49:41 -0600

    13 adds basic user schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { User } from './schemas/User';
 
 // eslint-disable-next-line prettier/prettier
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost:27017/keystone-store-tutorial';
@@ -23,6 +24,7 @@ export default config({
   },
   lists: createSchema({
     // schema items here
+    User,
   }),
   ui: {
     // TODO change for roles
