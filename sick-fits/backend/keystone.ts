45ce6010c689a3b387f4d1f30f88961a91d4208e
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-06-29 13:38:37 -0600

    10-12 hook up mongodb atlas, keystone and typescript in backend

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,32 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+// eslint-disable-next-line prettier/prettier
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost:27017/keystone-store-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 365,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // add dataseeding
+  },
+  lists: createSchema({
+    // schema items here
+  }),
+  ui: {
+    // TODO change for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO add session values
+});
