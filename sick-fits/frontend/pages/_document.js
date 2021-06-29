3e3a4416dc4ebafde6ee554adedcd32cf2d02fb9
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-06-29 11:46:46 -0600

    9 adds server side rendering of styled components to prevent potential flash of unstyled content

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -1,14 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  render() {
-    <Html>
-      <Head />
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>;
+  render() {
+    return (
+      <Html>
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
